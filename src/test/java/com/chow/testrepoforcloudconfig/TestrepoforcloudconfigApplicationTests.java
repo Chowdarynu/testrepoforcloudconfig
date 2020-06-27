8a45739ee7a7395a6a2f6c4eb954ba492dbea2f2
Chowdary <chowdarynu@gmail.com>
2020-06-27 22:23:11 +0530

    initiaal commit

---
+++ src/test/java/com/chow/testrepoforcloudconfig/TestrepoforcloudconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chow.testrepoforcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestrepoforcloudconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
